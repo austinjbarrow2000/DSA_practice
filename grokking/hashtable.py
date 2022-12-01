a7b6d55ff2323e28db51abdcf31bc046817d635f
Austin Barrow <austinjbarrow2000@gmail.com>
2022-11-30 23:40:13 -0500

    Add hashtable

---
+++ grokking/hashtable.py
@@ -0,0 +1,11 @@
+# phonebook = dict()
+phonebook = {}
+
+phonebook["Austin"] = 323
+phonebook["Sidd"] = 310
+phonebook["John"] = 627
+phonebook["Rasvik"] = 525
+
+print(phonebook["Austin"])
+print(phonebook.get("John"))
+print(phonebook.get("Yo"))
